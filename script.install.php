8242cc7cf69385c7bc59ab79f5bab3d0cc834392
Pascal Leconte <pascal.leconte@conseilgouz.com>
2024-11-15 09:08:08 +0100

    2.0.2
    Joomla 5.x Jokatree

--- script.install.php
+++ script.install.php
@@ -4,12 +4,14 @@
  * file		script.php
  *
  * @version	2.0.0
- * @author	Niels van Dantzig (2009-2014) - Robert Gastaud
+ * @author	Niels van Dantzig (2009-2014) - Robert Gastaud 
  * @package	Joomla
  * @subpackage	Joaktree
  * @license	GNU/GPL
  *
  * Component for genealogy in Joomla!
+ *
+ * Joomla! 5.x conversion by Conseilgouz
  *
  */
 // no direct access
