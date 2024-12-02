740cdca69af7841c6b0f22004b5a35db289590e3
Pascal Leconte <pascal.leconte@conseilgouz.com>
2024-12-02 17:28:55 +0100

    Update css_admin.js

--- packages/com_joaktree/media/js/iso/css_admin.js
+++ packages/com_joaktree/media/js/iso/css_admin.js
@@ -16,42 +16,48 @@ document.addEventListener('DOMContentLoaded', function() {
 
     sel = document.querySelector('#fieldset-basic');
     sel = sel.querySelector('.form-grid');
-    if (sel) sel.classList.remove('form-grid');
+    if (!sel) { return; }
+    sel.classList.remove('form-grid');
     sels = sel.querySelectorAll('.control-group .control-label');
     sels.forEach(function (element) {
         element.style.width = '140px';
     });   
-    sels = document.querySelectorAll('.clear');
+    sels = sel.querySelectorAll('.clear');
     sels.forEach(function (element) {
         element.parentNode.parentNode.parentNode.style.clear = 'both';
         element.parentNode.parentNode.style.clear = 'both';
     });
-    sels = document.querySelectorAll('input.left');
+    sels = sel.querySelectorAll('input.left');
     sels.forEach(function (element) {
         element.parentNode.parentNode.style.float = 'left';
     });
-    sels = document.querySelectorAll('div.left');
+    sels = sel.querySelectorAll('div.left');
     sels.forEach(function (element) {
         element.parentNode.parentNode.parentNode.style.float = 'left';
         element.parentNode.parentNode.style.float = 'left';
     });
-    sels = document.querySelectorAll('input.right');
+    sels = sel.querySelectorAll('input.right');
     sels.forEach(function (element) {
         element.parentNode.parentNode.style.float = 'right';
     });
 
-    sels = document.querySelectorAll('div.right');
+    sels = sel.querySelectorAll('div.right');
     sels.forEach(function (element) {
         element.parentNode.parentNode.parentNode.style.float = 'right';
         element.parentNode.parentNode.style.float = 'right';
     });
     
-    sels = document.querySelectorAll('input.half');
+    sels = sel.querySelectorAll('div.half.radio');
+    sels.forEach(function (element) {
+        element.parentNode.parentNode.parentNode.style.width = '50%';
+        element.parentNode.parentNode.style.width = '50%';
+    });
+    sels = sel.querySelectorAll('input.half');
     sels.forEach(function (element) {
         element.parentNode.parentNode.style.width = '50%';
         element.parentNode.style.width = '50%';
     });
-    sels = document.querySelectorAll('div.half');
+    sels = sel.querySelectorAll('div.half:not(.radio)');
     sels.forEach(function (element) {
         element.parentNode.parentNode.parentNode.style.width = '50%';
         element.parentNode.parentNode.style.width = '50%';
